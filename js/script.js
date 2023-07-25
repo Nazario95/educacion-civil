5d5b676681acde6e23f6f206f89a13134ead8f40
Nazario Muanbama <57312608+Nazario95@users.noreply.github.com>
2023-07-25 18:20:26 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,32 @@ import tablaLibros from "../db-Json/tabla-libros.json" assert{type:"json"}
 window.addEventListener('DOMContentLoaded',()=> {    
     
     var btnBuscar = document.querySelector('a.realizar-busqueda'); 
+    
+    function cargarContenidoJson(){
+    //variables seccion nuevos libros
+    var idLibro, tituloLibro, fechaSubida, enlace;
+    var filatabla = document.querySelector('tbody.elementos-fila');
+    var menuEncabezado = document.querySelector('ul.menu-encabezado');
+    //variables seccion mas leidos
+    var idLibroMasLeido, tituloLibroMasLeido, enlaceLibroMasleido;
 
+    // CARGAR LISTA DE LIBROS EN EL HEADER    
+        for(var i=0; i<tablaLibros.length; i++){
+            idLibro = tablaLibros[i].id;
+            tituloLibro = tablaLibros[i].nombreLibro;
+            fechaSubida = tablaLibros[i].fecha;
+            enlace = tablaLibros[i].enlace;
+
+            var elmentosLista = document.createElement('LI');
+            var enlaceLista = document.createElement('A');
+        
+            enlaceLista.className = "dropdown-item";
+            enlaceLista.href = enlace;
+            enlaceLista.textContent = tituloLibro;
+        
+            elmentosLista.appendChild(enlaceLista);
+            menuEncabezado.appendChild(elmentosLista);
+        } 
     btnBuscar.addEventListener('click',()=>{
         var valoresEncontrados = 0;
         var capturarValorinput = document.querySelector('input').value;
